b5cccff1e1793df404bd7ce1c55ce9ce57ceccb8
kimbab90 <kimbab90@naver.com>
2025-03-14 12:31:04 +0900

    first commit

---
+++ src/main/java/com/candle/FrontController.java
@@ -0,0 +1,46 @@
+package com.candle;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.candle.common.Control;
+
+
+public class FrontController extends HttpServlet {
+	Map<String, Control> map;
+
+	public FrontController() {
+		map = new HashMap<>();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// map.put("/main.do", new MainControl());
+		// 김민식
+		
+		// 김어진
+		
+		// 정은총
+		
+		// 김지수
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) //
+			throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		String context = req.getContextPath();
+		String path = uri.substring(context.length());
+
+		Control sub = map.get(path);
+		sub.exec(req, resp);
+
+	}
+}
