53be3a4cbd4a367f38c508e4ca210f7b7c6ace2a
kimbab90 <kimbab90@naver.com>
2025-03-17 10:33:07 +0900

    new project

---
+++ src/src/main/java/com/candle/mapper/CandleMapper.java
@@ -0,0 +1,5 @@
+package com.candle.mapper;
+
+public class CandleMapper {
+
+}
---
+++ src/src/main/java/com/candle/vo/SearchVO.java
@@ -0,0 +1,16 @@
+package com.candle.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchVO {
+	private int page;
+	private String searchCondition;
+	private String keyword;
+	private int itemNo;
+}
