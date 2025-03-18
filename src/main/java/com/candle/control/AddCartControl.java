3c8a77cf4b5fa8ba41aa070546b9d0cf1eb81fd8
김어진 <asas0066@naver.com>
2025-03-18 22:12:08 +0900

    h250318

--- src/main/java/com/candle/control/AddCartControl.java
+++ src/main/java/com/candle/control/AddCartControl.java
@@ -25,24 +25,27 @@ public class AddCartControl implements Control {
 		SqlSession sqlSession = DataSource.getInstance().openSession();
 		EohMapper mapper = sqlSession.getMapper(EohMapper.class);
 		
+		Integer orderNoObj = mapper.orderNo(userNo);
+		int orderNo = (orderNoObj != null) ? orderNoObj : 0;
+		
 		//orderNo check
-		int orderNo = mapper.orderNo(userNo); // orderNo의 정보가 있는지 없는지 확인.
+//		int orderNo = mapper.orderNo(userNo); // orderNo의 정보가 있는지 없는지 확인.
 		// 0 이면
-		if (orderNo == 0) {
-		AddCartVO addCart = new AddCartVO();
+		if (orderNo == 0) { // 주문이 없으면.
+		mapper.orderNo(userNo); // 새로운 주문을 추가하고 생성된 orderNo를 받아옴.
+		orderNo = mapper.orderNo(userNo); // 새롭게 생성된 주문번호 가져오기.	
+		} 
+		
+		// 있으면 candle_order_item 에 insert
+		AddCartVO addCart = new AddCartVO(); // orderNo != 0 이면, 새로운 주문을 추가.
 		addCart.setOrderNo(orderNo);
 		addCart.setUserNo(userNo);
 		addCart.setItemNo(itemNo);
 		
-		
 		int result = mapper.orderInfo(addCart);
-		} 
-		
-		// 있으면 candle_order_item 에 insert
-		
 		
 //		req.setAttribute("product", product);
-		req.getRequestDispatcher("product/addCartInfo.tiles").forward(req, resp);
+//		req.getRequestDispatcher("product/addCartInfo.tiles").forward(req, resp);
 		
 		// 주문정보 candle_order insert
 		
