83374b6ede70c91b16d3dbd26f2b68f7cdb1a3c2
ddul22 <kjj0757@naver.com>
2025-03-20 05:46:29 +0900

    250319

---
+++ src/main/java/com/candle/control/QnaAddControl.java
@@ -0,0 +1,48 @@
+package com.candle.control;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.candle.common.Control;
+import com.candle.common.DataSource;
+import com.candle.mapper.JisuMapper;
+import com.candle.vo.QnaVO;
+
+public class QnaAddControl implements Control {
+
+	@Override
+	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String title = req.getParameter("qnaTitle");
+		String content = req.getParameter("qnaContent");
+		String userNo = req.getParameter("userNo");
+		
+		Map<String, Object> addQna = new HashMap<String, Object>();
+		addQna.put("qnaTitle", title);
+		addQna.put("qnaContent", content);
+		addQna.put("userNo", userNo);
+		
+		SqlSession sqlSession=DataSource.getInstance().openSession(true);
+		JisuMapper mapper = sqlSession.getMapper(JisuMapper.class);
+		
+		int result = mapper.insertUser(addQna);
+		
+		if(result>0) {
+			HttpSession session = req.getSession();
+			resp.sendRedirect("qnaAddForm.do"); 
+
+		}else {
+			resp.getWriter().print("{\"retCode\": \"NG\"}"); 
+		}
+		}
+
+
+}
